026f60c771bd603338dfbc178f972c2b9781fb55
Matej Lamza <matej.lamza@decode.agency>
2023-08-24 11:58:10 +0200

    Updated min sdk to 21 because Appcenter was trhowing error

--- android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -4,6 +4,7 @@ import io.flutter.embedding.android.FlutterActivity
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
+import android.os.Bundle
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
