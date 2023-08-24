2ed997221359073fd67276048f1ad9e9b4f5c75e
Matej Lamza <matej.lamza@decode.agency>
2023-08-24 11:24:20 +0200

    Add appcenter dependencies, upgraded java to 11,

--- android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.example.verygoodcore.pipeline
 
 import io.flutter.embedding.android.FlutterActivity
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCenter.start(
+            getApplication(), "2b5e0277-046c-4dc7-9c98-ecdf6b3ed70a",
+            Analytics::class.java, Crashes::class.java
+        )
+    }
 }
