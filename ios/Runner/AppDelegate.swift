8b49cc163f6d97bd17396f4af4ed62ae366589c1
Matej Lamza <matej.lamza@decode.agency>
2023-08-25 12:58:58 +0200

    added ios code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,8 @@
 import UIKit
 import Flutter
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +11,10 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    AppCenter.start(withAppSecret: "0df79d87-755c-48b7-9174-835735803f62", services:[
+      Analytics.self,
+      Crashes.self
+    ])
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,34 @@
+import 'package:flutter_appcenter_bundle/flutter_appcenter_bundle.dart';
 import 'package:pipeline/app/app.dart';
 import 'package:pipeline/bootstrap.dart';
 
 void main() {
+  appCenter();
   bootstrap(() => const App());
+}
+
+Future<void> appCenter() async {
+  await AppCenter.startAsync(
+    appSecretAndroid: '2b5e0277-046c-4dc7-9c98-ecdf6b3ed70a',
+    //YOUR android APPSECRET CODE
+    appSecretIOS: '0df79d87-755c-48b7-9174-835735803f62',
+    //YOUR iOS APPSECRET CODE
+    enableAnalytics: true,
+    // Defaults to true
+    enableCrashes: true,
+    // Defaults to true
+    enableDistribute: true,
+    // Defaults to false
+    usePrivateDistributeTrack: false,
+    // Defaults to false
+    disableAutomaticCheckForUpdate: false, // Defaults to false
+  );
+
+  await AppCenter.configureDistributeDebugAsync(enabled: false);
+  await AppCenter.configureAnalyticsAsync(enabled: true);
+  await AppCenter.configureCrashesAsync(enabled: true);
+  await AppCenter.configureDistributeAsync(enabled: true);
+  await AppCenter.configureDistributeDebugAsync(enabled: true);
+  // Android Only
+  await AppCenter.checkForUpdateAsync(); // Manually check for update
 }
