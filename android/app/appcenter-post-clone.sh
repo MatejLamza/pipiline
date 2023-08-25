47afed81d3ce3657ec834ccca14fcc7ac331a467
Matej Lamza <matej.lamza@decode.agency>
2023-08-25 12:12:48 +0200

    updated post clone script

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -19,6 +19,6 @@ flutter build apk --release -t lib/main.dart
 # if you need build bundle (AAB) in addition to your APK, uncomment line below and last line of this script.
 #flutter build appbundle --release --build-number $APPCENTER_BUILD_ID
 # copy the APK where AppCenter will find it
-mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
+mkdir -p android/app/build/outputs/apk/; mv android/app/build/outputs/apk/app-release.apk $_
 # copy the AAB where AppCenter will find it
 #mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/release/app-release.aab $_
